84a88e35b2a8938de83867930665de56c107d225
Paul Batterman <pbatterman@gmail.com>
2017-02-14 16:07:26 -0500

    tonnetz grid

--- js/tonnetz.js
+++ js/tonnetz.js
@@ -30,15 +30,25 @@ function init() {
     scene.add(light);
  
    	var geom = new THREE.Geometry(); 
-	var v1 = new THREE.Vector3(0,0,0);
-	var v2 = new THREE.Vector3(0,500,0);
-	var v3 = new THREE.Vector3(0,500,500);
 
-	geom.vertices.push(v1);
-	geom.vertices.push(v2);
-	geom.vertices.push(v3);
+   	for (var y = 0; y < 6; y++) {
+   		for (var x = 0; x < 7; x++) {
+   			var vertex = new THREE.Vector3(100*x + 50*y, 100*y, 0);
+   			geom.vertices.push(vertex);
+   		}
+   	}
 
-	geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	var normal = new THREE.Vector3( 0, 1, 0 ); //optional
+   	for (var v = 0; v < geom.vertices.length - 2; v++) {
+
+   		var color = new THREE.Color( 0xff0000 );
+   		if ( v % 2 == 0) {
+   			color = new THREE.Color( 0x0000ff );
+   		}
+   		geom.faces.push( new THREE.Face3( v, v+1, v+2, normal, color ) );
+   	}
+
+	//geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
 
 	var object = new THREE.Mesh( geom, new THREE.MeshNormalMaterial() );
 	scene.add(object);
