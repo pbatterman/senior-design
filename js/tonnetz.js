4276d36ce47bb953afa3179ece35a33582a27aae
Paul Batterman <pbatterman@gmail.com>
2017-02-14 15:25:20 -0500

    more threejs addition

--- js/tonnetz.js
+++ js/tonnetz.js
@@ -29,5 +29,28 @@ function init() {
     light.position.set(-100,200,100);
     scene.add(light);
  
+   	var geom = new THREE.Geometry(); 
+	var v1 = new THREE.Vector3(0,0,0);
+	var v2 = new THREE.Vector3(0,500,0);
+	var v3 = new THREE.Vector3(0,500,500);
 
+	geom.vertices.push(v1);
+	geom.vertices.push(v2);
+	geom.vertices.push(v3);
+
+	geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+
+	var object = new THREE.Mesh( geom, new THREE.MeshNormalMaterial() );
+	scene.addObject(object);
+
+
+ 	controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+ 
+    // Render the scene.
+    renderer.render(scene, camera);
+    controls.update();
 }
