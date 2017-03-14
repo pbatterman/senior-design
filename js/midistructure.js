141e3b33ed22c6697708dc727fe199c108ce918b
Paul Batterman <pbatterman@gmail.com>
2017-03-14 19:51:43 -0400

    continue notes

--- js/midistructure.js
+++ js/midistructure.js
@@ -1,15 +1,20 @@
 // convert array to threejs vertices for point cloud render
-function createGeometry(noteArray) {
+function createGeometry(noteArray, totalTime) {
 	var geometry = new THREE.Geometry();
 	var zidx = 0;
- 	for (var eventTime in noteArray) {
-		var notes = noteArray[eventTime];
+	var notes = noteArray[0];
+ 	// for (var eventTime in noteArray) {
+ 	for (var t = 0; t < totalTime; t++) {
+ 		if (noteArray[t]) {
+ 			notes = noteArray[t];
+ 		}
+		// var notes = noteArray[eventTime];
 		for (var n = 0; n < notes.length; n++) {
 			var note = notes[n];
  			for (var x = 0; x < 7; x++) {
 				for (var y = 0; y < 6; y++) {
 					if (noteVertices[x][y] == note) {
-						var vertex = new THREE.Vector3(100*x + 50*y, 500 - (100*y), 0.15*eventTime);
+						var vertex = new THREE.Vector3(100*x + 50*y, 500 - (100*y), t);
 						// var vertex = new THREE.Vector3(x, y, 0);
 						geometry.vertices.push(vertex);
 						var col = HSLArrayToString(gColorMap[note]);
@@ -51,6 +56,6 @@ function buildSongStructure(midifile) {
 		notesForStruct[totalTime] = currentNotes.slice();
 
 	}
-	var geometry = createGeometry(notesForStruct);
+	var geometry = createGeometry(notesForStruct, totalTime);
 	return geometry;
 }
